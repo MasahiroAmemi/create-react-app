ae02ed66cd00d8758f90add24c4a91a51878f8c8
MASAHIRO AMEMIYA <ame6masa6@gmail.com>
2022-09-18 15:47:29 +0900

    react-portfolio-site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,31 @@
+// 画像をそれぞれimportします。パスに注意。
+import CoverImage from '../images/cover-image.jpg';
+import ProfileImage from '../images/profile-image.png';
+
 export const Header = () => {
-  const componentName = () => "Header";
-  return(
-    <div className="test-wrapper">
-      <p className="text">
-      このコンポーネントの名前は{componentName()}です。
-      </p>
-    </div>
+  return (
+    <header className="main-cover" style={{backgroundImage: `url(${CoverImage})`}}>
+      {/* overlayはカバー画像の上に透過して表示される背景要素 */}
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            {/* カバー画像 */}
+            <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
+            {/* 名前や肩書を自由に入れる */}
+            <h1 className="title-text">雨宮</h1>
+            <h3 className="title-text">初心者です(ex.Engineer)</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
